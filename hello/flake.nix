67de7f93a51107f8b012ed7d36c349a4b4a76e9d
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-17 19:25:28 -0500

    Intermediate state

--- hello/flake.nix
+++ hello/flake.nix
@@ -1,21 +1,23 @@
 {
-  description = "Simple C program";
+  description = "Hello C binary flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
   outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      hello = pkgs.stdenv.mkDerivation {
+      pkgs   = import nixpkgs { inherit system; };
+      hello  = pkgs.stdenv.mkDerivation {
         pname = "hello";
         version = "1.0";
         src = builtins.fetchGit {
           url = "https://github.com/notarealdeveloper/hello.git";
+          ref = "master";
           rev = "aea1e7c2100d5c1c3c3bcf910c00a247930135e6";
-          ref = "refs/heads/master";
         };
-        buildInputs = [ pkgs.gnumake pkgs.gcc ];
+        nativeBuildInputs = [ pkgs.gnumake pkgs.gcc ];
         buildPhase = "make";
         installPhase = ''
           mkdir -p $out/bin
@@ -24,8 +26,20 @@
       };
     in {
       packages = {
-        x86_64-linux = {
-          default = hello;
+        "${system}" = {
+          hello         = hello;
+          default       = hello;              # <-- makes `.default` available
+        };
+      };
+
+      defaultPackage.${system} = hello;      # <-- also works
+
+      apps = {
+        "${system}" = {
+          hello = {
+            type    = "app";
+            program = "${hello}/bin/hello";
+          };
         };
       };
     };
