a6b1f98fc076ba9ce5544cbefbfe208a0b140c6e
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-18 13:06:12 -0500

    Giving up again. For now.

--- jello/flake.nix
+++ jello/flake.nix
@@ -1,41 +1,34 @@
 {
-  description = "Python jello package with GitHub‑based C hello dependency";
+  description = "jello flake, runtime depends on hello";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    selfpkgs.url = "github:notarealdeveloper/flakes/master";
+    hello = { url = "github:notarealdeveloper/helloflake/master"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs = { self, nixpkgs, selfpkgs, ... } @ inputs:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    mine = import selfpkgs {};
+  outputs = { self, nixpkgs, hello, ... }:
+    let
+      system = "x86_64-linux";  # static target
+      pkgs = import nixpkgs { inherit system; };
+      helloPkg = hello.packages.${system}.default;
+    in {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "jello";
+        version = "1.0";
 
-    # Python package: uses setuptools via PEP-517 hooks
-    jello = pkgs.python3Packages.buildPythonPackage rec {
-      pname = "jello";
-      version = "1.0";
+        src = ./.;  # **must** point to your source directory
 
-      src = builtins.fetchGit {
-        url = "https://github.com/notarealdeveloper/jello.git";
-        ref = "master";
-        rev = "18d6bc75b6d16133ca8b5ebd3db486da36cbecc9";
+        nativeBuildInputs = [
+          pkgs.gnumake
+          pkgs.nixVersions.stable
+        ];
+
+        buildInputs = [ helloPkg pkgs.python3Packages.pip ];
+        propagatedBuildInputs = [ helloPkg ];
+
+        buildPhase = "make";
+        installPhase = "make install";
       };
-
-      format = "pyproject";
-
-      nativeBuildInputs = [ pkgs.gnumake ];
-      buildInputs = [
-        mine.hello
-        pkgs.python3Packages.pip
-      ];
-      buildPhase = "make";
-      installPhase = "make install";
-
     };
-  in {
-    packages.${system}.default = jello;
-  };
 }
 
