c10d9a7b11d2dac2ea051a2d00a3166eb4370e3e
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-18 11:49:38 -0500

    Experiment: Creating a new package set so we can use the hello flake inside jello

--- jello/flake.nix
+++ jello/flake.nix
@@ -3,29 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    selfpkgs.url = "github:notarealdeveloper/flakes/master"
   };
 
   outputs = { self, nixpkgs, ... }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
-
-    # C dependency: pinned commit from GitHub
-    hello = pkgs.stdenv.mkDerivation {
-      pname = "hello";
-      version = "1.0";
-      src = builtins.fetchGit {
-        url = "https://github.com/notarealdeveloper/hello.git";
-        ref = "master";
-        rev = "aea1e7c2100d5c1c3c3bcf910c00a247930135e6";
-      };
-      nativeBuildInputs = [ pkgs.gnumake pkgs.gcc ];
-      buildPhase = "make";
-      installPhase = ''
-        mkdir -p $out/bin
-        cp hello $out/bin/
-      '';
-    };
+    mine = import selfpkgs {};
 
     # Python package: uses setuptools via PEP-517 hooks
     jello = pkgs.python3Packages.buildPythonPackage rec {
@@ -42,26 +27,15 @@
 
       nativeBuildInputs = [ pkgs.gnumake ];
       buildInputs = [
-        hello
-        pkgs.python3Packages.setuptools
-        pkgs.python3Packages.wheel
+        mine.hello
+        pkgs.python3Packages.pip
       ];
+      buildPhase = "make";
+      installPhase = "make install";
 
-      # Ensure pytest is available during test-time
-      nativeCheckInputs = [ pkgs.python3Packages.pytest ];
-
-      doCheck = false;
     };
   in {
-    packages.${system} = {
-      hello = hello;
-      jello = jello;
-      default = jello;
-    };
-
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = [ hello jello ];
-    };
+    packages.${system}.default = jello;
   };
 }
 
